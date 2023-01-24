9a7d803165a562e4a5fb055479ec4cdda4b8bcac
dismas <afubwa@gmail.com>
2023-01-24 10:59:20 +0400

    Dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+
+
+// # Task 1 Removing the main Element Through Dom
+
+
+const main = document.querySelector('main#main')
+
+main.parentNode.removeChild(main);
+
+
+// # Task 2 Adding New Header
+
+const newHeader = document.createElement("h1");
+
+// # Task 3 Adding ID to New Header 
+
+newHeader.id = "victory";
+
+// # Task 4 Adding NAme
+
+let name = 'Dismas';
+newHeader.innerHTML = `${name} is the champion`;
